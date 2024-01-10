9fbd57e67e690e91291fe5d376ce9da7b0c95b41
Ken Lin <szukyu@Kens-Air.lan>
2024-01-09 19:42:51 -0500

    debugging

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ document.getElementById("btn1").addEventListener("click", function () {
     activeButton();
 });
 
-function btnFunc2() {
+document.getElementById("btn2").addEventListener("click", function () {
     document.getElementById("btn2").classList.add("activeBtn");
     document.getElementById("btn2").classList.remove("deactivatedBtn");
     document.getElementById("btn1").classList.add("deactivatedBtn");
@@ -20,9 +20,9 @@ function btnFunc2() {
     document.getElementById("btn4").classList.add("deactivatedBtn");
     document.getElementById("btn4").classList.remove("activeBtn");
     activeButton();
-}
+});
 
-function btnFunc3() {
+document.getElementById("btn3").addEventListener("click", function () {
     document.getElementById("btn3").classList.add("activeBtn");
     document.getElementById("btn3").classList.remove("deactivatedBtn");
     document.getElementById("btn2").classList.add("deactivatedBtn");
@@ -32,9 +32,9 @@ function btnFunc3() {
     document.getElementById("btn4").classList.add("deactivatedBtn");
     document.getElementById("btn4").classList.remove("activeBtn");
     activeButton();
-}
+});
 
-function btnFunc4() {
+document.getElementById("btn4").addEventListener("click", function () {
     document.getElementById("btn4").classList.add("activeBtn");
     document.getElementById("btn4").classList.remove("deactivatedBtn");
     document.getElementById("btn2").classList.add("deactivatedBtn");
@@ -44,7 +44,7 @@ function btnFunc4() {
     document.getElementById("btn1").classList.add("deactivatedBtn");
     document.getElementById("btn1").classList.remove("activeBtn");
     activeButton();
-}
+});
 
 async function activeButton() {
     var button = document.querySelector('.activeBtn');
@@ -55,7 +55,7 @@ async function activeButton() {
 
 let str, int, luck;
 
-function statGeneration() {
+document.getElementById("statBtn").addEventListener("click", function () {
     var button = document.querySelector('.activeBtn');
     if (button.value == "Hasagi") {
         str = Math.floor(Math.random() * 5 + 15)
@@ -77,6 +77,6 @@ function statGeneration() {
         str = Math.floor(Math.random() * 5 + 7)
         luck = Math.floor(Math.random() * 5 + 7)
     }
-}
+});
 
 export {str, int, luck}
