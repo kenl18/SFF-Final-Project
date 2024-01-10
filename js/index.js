813553deee79ffb48e93619f249226a83a624810
Ken Lin <szukyu@Kens-Air.lan>
2024-01-09 20:11:17 -0500

    debugging

--- js/index.js
+++ js/index.js
@@ -1,3 +1,38 @@
+async function activeButton() {
+    var button = document.querySelector('.activeBtn');
+    var text = document.querySelector('.chosenClass');
+    console.log(button.value);
+    text.innerHTML = "Chosen Class: " + button.value;
+}
+
+let str, int, luck;
+
+document.getElementById("statBtn").addEventListener("click", function () {
+    var button = document.querySelector('.activeBtn');
+    if (button.value == "Hasagi") {
+        str = Math.floor(Math.random() * 5 + 15)
+        int = Math.floor(Math.random() * 5 + 3)
+        luck = Math.floor(Math.random() * 5 + 3)
+    }
+    if (button.value == "Aldous") {
+        int = Math.floor(Math.random() * 5 + 15)
+        str = Math.floor(Math.random() * 5 + 3)
+        luck = Math.floor(Math.random() * 5 + 3)
+    }
+    if (button.value == "Geraldo") {
+        luck = Math.floor(Math.random() * 5 + 15)
+        str = Math.floor(Math.random() * 5 + 3)
+        int = Math.floor(Math.random() * 5 + 3)
+    }
+    if (button.value == "Jack") {
+        int = Math.floor(Math.random() * 5 + 7)
+        str = Math.floor(Math.random() * 5 + 7)
+        luck = Math.floor(Math.random() * 5 + 7)
+    }
+});
+
+export {str, int, luck};
+
 document.getElementById("btn1").addEventListener("click", function () {
     document.getElementById("btn1").classList.add("activeBtn");
     document.getElementById("btn1").classList.remove("deactivatedBtn");
@@ -45,38 +80,3 @@ document.getElementById("btn4").addEventListener("click", function () {
     document.getElementById("btn1").classList.remove("activeBtn");
     activeButton();
 });
-
-async function activeButton() {
-    var button = document.querySelector('.activeBtn');
-    var text = document.querySelector('.chosenClass');
-    console.log(button.value);
-    text.innerHTML = "Chosen Class: " + button.value;
-}
-
-let str, int, luck;
-
-document.getElementById("statBtn").addEventListener("click", function () {
-    var button = document.querySelector('.activeBtn');
-    if (button.value == "Hasagi") {
-        str = Math.floor(Math.random() * 5 + 15)
-        int = Math.floor(Math.random() * 5 + 3)
-        luck = Math.floor(Math.random() * 5 + 3)
-    }
-    if (button.value == "Aldous") {
-        int = Math.floor(Math.random() * 5 + 15)
-        str = Math.floor(Math.random() * 5 + 3)
-        luck = Math.floor(Math.random() * 5 + 3)
-    }
-    if (button.value == "Geraldo") {
-        luck = Math.floor(Math.random() * 5 + 15)
-        str = Math.floor(Math.random() * 5 + 3)
-        int = Math.floor(Math.random() * 5 + 3)
-    }
-    if (button.value == "Jack") {
-        int = Math.floor(Math.random() * 5 + 7)
-        str = Math.floor(Math.random() * 5 + 7)
-        luck = Math.floor(Math.random() * 5 + 7)
-    }
-});
-
-export {str, int, luck};
