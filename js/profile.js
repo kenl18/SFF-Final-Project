76b652a3d4b89692afdfcc196ad23c86dd224345
Ken Lin <szukyu@Kens-Air.lan>
2024-01-09 19:45:07 -0500

    debugging

--- js/profile.js
+++ js/profile.js
@@ -1,7 +1,7 @@
 const {str, int, luck} = require('.index.js');
 var strText = document.querySelector(".str")
-strText.innerHTML = "Strength: " = str;
+strText.innerHTML = "Strength: " + str;
 var intText = document.querySelector(".int")
-intText.innerHTML = "Intelligence: " = int;
+intText.innerHTML = "Intelligence: " + int;
 var luckText = document.querySelector(".luck")
-luckText.innerHTML = "Luck: " = luck;
+luckText.innerHTML = "Luck: " + luck;
