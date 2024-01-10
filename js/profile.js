61bc9024a4dec9e26922b5f2c89cde3528917c40
Ken Lin <szukyu@Kens-Air.lan>
2024-01-09 20:43:21 -0500

    Working hopefully

--- js/profile.js
+++ js/profile.js
@@ -1,6 +1,18 @@
+function getCookieValue(name) {
+    const cookies = document.cookie.split('; ');
+    for (const cookie of cookies) {
+        const [cookieName, cookieValue] = cookie.split('=');
+        if (cookieName === name) {
+            return cookieValue;
+        }
+    }
+    return null;
+}
+
 const str = getCookieValue('str');
 const int = getCookieValue('int');
 const luck = getCookieValue('luck');
+
 
 console.log(str);
 var strText = document.querySelector(".str")
