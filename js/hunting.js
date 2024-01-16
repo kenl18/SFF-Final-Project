6121988d96294eabb7afb4d2d15eae57d121bdd5
kenl18 <124067550+kenl18@users.noreply.github.com>
2024-01-16 16:10:34 -0500

    asd

--- js/hunting.js
+++ js/hunting.js
@@ -1,6 +1,18 @@
+function getCookieValue(name) {
+    const cookies = document.cookie.split('; ');
+    for (const cookie of cookies) {
+        const [cookieName, cookieValue] = cookie.split('=');
+        if (cookieName === name) {
+            return cookieValue;
+        }
+    }
+    return null;
+}
+
+var luck = parseInt(getCookieValue('luck'));
+
 document.getElementById("statBtn").addEventListener("click", function () {
     const randomNum = Math.floor(Math.random() * 100) + 1;
-    var luck = parseInt(getCookieValue('luck'));
 
     if (randomNum <= 0.25 * luck) {
         window.location.href = 'https://kenl18.github.io/SFF-Final-Project/html/fishW.html';
@@ -8,3 +20,8 @@ document.getElementById("statBtn").addEventListener("click", function () {
         window.location.href = 'https://kenl18.github.io/SFF-Final-Project/html/fishL.html';
     }
 });
+
+function chance() {
+    var footer = document.querySelector(".footer");
+    footer.innerHTML = `Scales off luck: ${0.25 * luck}% of success`
+}
