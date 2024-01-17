ddb6b69f74af9999d24859643196455cc4c285e4
kenl18 <124067550+kenl18@users.noreply.github.com>
2024-01-17 08:07:08 -0500

    Update dragon.js

--- js/dragon.js
+++ js/dragon.js
@@ -1,1 +1,21 @@
+function getCookieValue(name) {
+    const cookies = document.cookie.split('; ');
+    for (const cookie of cookies) {
+        const [cookieName, cookieValue] = cookie.split('=');
+        if (cookieName === name) {
+            return cookieValue;
+        }
+    }
+    return null;
+}
 
+document.getElementById("statBtn").addEventListener("click", function () {
+    const randomNum = Math.floor(Math.random() * 100) + 1;
+    var int = parseInt(getCookieValue('int'));
+
+    if (randomNum <= 2.5 * int) {
+        window.location.href = 'https://kenl18.github.io/SFF-Final-Project/html/stealW.html';
+    } else {
+        window.location.href = 'https://kenl18.github.io/SFF-Final-Project/html/stealL.html';
+    }
+});
