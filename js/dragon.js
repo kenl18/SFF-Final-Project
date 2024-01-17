62f7e0f8007aa608db4bff9db55d349a862caac2
kenl18 <124067550+kenl18@users.noreply.github.com>
2024-01-17 10:49:03 -0500

    Update dragon.js

--- js/dragon.js
+++ js/dragon.js
@@ -9,13 +9,13 @@ function getCookieValue(name) {
     return null;
 }
 
+var int = parseInt(getCookieValue('int'));
+var str = parseInt(getCookieValue('str'));
+var luck = parseInt(getCookieValue('luck'));
+
 document.getElementById("statBtn").addEventListener("click", function () {
     const randomNum = Math.floor(Math.random() * 100) + 1;
-    var int = parseInt(getCookieValue('int'));
-    var str = parseInt(getCookieValue('str'));
-    var luck = parseInt(getCookieValue('luck'));
-
-
+    
     if (randomNum <= 0.125 * (luck + int + (2 * str))) {
         window.location.href = 'https://kenl18.github.io/SFF-Final-Project/html/dragonfightW.html';
     } else {
