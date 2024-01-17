76597ee6733b3281117e1b73b9d9edc1bce0117d
Ken Lin <szukyu@Kens-Air.lan>
2024-01-16 19:49:34 -0500

    Js

---
+++ js/leftpath.js
@@ -0,0 +1,32 @@
+function getCookieValue(name) {
+    const cookies = document.cookie.split('; ');
+    for (const cookie of cookies) {
+        const [cookieName, cookieValue] = cookie.split('=');
+        if (cookieName === name) {
+            return cookieValue;
+        }
+    }
+    return null;
+}
+
+document.getElementById("fightBtn").addEventListener("click", function () {
+    const randomNum = Math.floor(Math.random() * 100) + 1;
+    var str = parseInt(getCookieValue('str'));
+
+    if (randomNum <= 1.25 * str) {
+        window.location.href = 'https://kenl18.github.io/SFF-Final-Project/html/elffightW.html';
+    } else {
+        window.location.href = 'https://kenl18.github.io/SFF-Final-Project/html/elffightL.html';
+    }
+});
+
+document.getElementById("talkBtn").addEventListener("click", function () {
+    const randomNum = Math.floor(Math.random() * 100) + 1;
+    var int = parseInt(getCookieValue('int'));
+
+    if (randomNum <= 1.25 * int) {
+        window.location.href = 'https://kenl18.github.io/SFF-Final-Project/html/elftalkW.html';
+    } else {
+        window.location.href = 'https://kenl18.github.io/SFF-Final-Project/html/elftalkL.html';
+    }
+});
